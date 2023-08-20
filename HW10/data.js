aaaf04ccbf568afe881d55d439706b728a62b662
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-08-20 14:36:47 +0300

    i

---
+++ HW10/data.js
@@ -0,0 +1,26 @@
+export const dataIn = `
+[
+       
+      {
+         "quote_id" : 1,
+         "quote" : "I am not in danger, Skyler. I am the danger!",
+         "author" : "Walter White",
+         "series" : "Breaking Bad"
+         "img" : "https://m.media-amazon.com/images/M/MV5BYmQ4YWMxYjUtNjZmYi00MDQ1LWFjMjMtNjA5ZDdiYjdiODU5XkEyXkFqcGdeQXVyMTMzNDExODE5._V1_.jpg"
+      },
+      {
+         "quote_id" : 2,
+         "quote" : "Stay out of my territory.",
+         "author" : "Walter White",
+         "series" : "Breaking Bad"
+         "img" : "https://m.media-amazon.com/images/M/MV5BYmQ4YWMxYjUtNjZmYi00MDQ1LWFjMjMtNjA5ZDdiYjdiODU5XkEyXkFqcGdeQXVyMTMzNDExODE5._V1_.jpg"
+      },
+      {
+         "quote_id" : 3,
+         "quote" : "IFT",
+         "author" : "Skyler White",
+         "series" : "Breaking Bad"
+         "img" : "https://m.media-amazon.com/images/M/MV5BYmQ4YWMxYjUtNjZmYi00MDQ1LWFjMjMtNjA5ZDdiYjdiODU5XkEyXkFqcGdeQXVyMTMzNDExODE5._V1_.jpg"
+      }
+]
+`
